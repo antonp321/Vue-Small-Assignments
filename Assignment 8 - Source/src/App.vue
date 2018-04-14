4ac6789a4bc2a20b8276502e6d43f178514ee657
Anton Papazov <antonp1p2@gmail.com>
2018-04-14 18:42:41 +0300

    latests

---
+++ Assignment 8 - Source/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="loadBlue">Load Blue Template</button>
+                <button class="btn btn-success" @click="loadGreen">Load Green Template</button>
+                <button class="btn btn-danger" @click="loadRed">Load Red Template</button>
+                <hr>
+                <component :is="loadedComponent">
+                    <h1 slot="blue">Blue</h1>
+                    <h1 slot="red">Red</h1>
+                    <h1 slot="green">Green</h1>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: function(){
+            return {
+                loadedComponent: "appBlue"
+            }
+        },
+        methods: {
+            loadBlue(){
+                this.loadedComponent = "appBlue";
+            },
+            loadGreen(){
+                this.loadedComponent = "appGreen";
+            },
+            loadRed(){
+                this.loadedComponent = "appRed";
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
