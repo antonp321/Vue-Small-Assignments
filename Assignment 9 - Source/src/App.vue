9cc618bf1c63a435796b1c48de7ab1e9c09854ce
Anton Papazov <antonp1p2@gmail.com>
2018-04-16 03:44:23 +0300

    latest

---
+++ Assignment 9 - Source/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <form v-if="!isSubmitted">
+                        <full-name v-model="formData.fName"></full-name><br/>
+                        Mail: <input type="text" v-model="formData.mail"><br/>
+                        Password: <input type="password" v-model="formData.pass"><br/>
+                        Store Data: Yes <input type="radio" value="Yes" v-model="formData.storeIt"> No <input type="radio" value="No" v-model="formData.storeIt"><br/><br/>
+                        <button @click.prevent="submitTheForm">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{formData.fName}}</p>
+                        <p>Mail: {{formData.mail}}</p>
+                        <p>Password: {{formData.pass}}</p>
+                        <p>Store in Database?: {{formData.storeIt}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import FullName from './FullName.vue';
+
+    export default {
+        components: {
+          'full-name': FullName
+        },
+        data(){
+            return {
+                formData: {
+                    fName: "",
+                    mail: "",
+                    "pass": "",
+                    storeIt: "No"
+                },
+                isSubmitted: false
+            }
+        },
+        methods: {
+            submitTheForm(){
+                this.isSubmitted = true;
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Assignment 9 - Source/src/FullName.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        First Name: <input type="text" :value="fName" @input="changeName(true, $event)">
+        Last Name: <input type="text" :value="lName" @input="changeName(false, $event)">
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['value'],
+        methods: {
+          changeName(isFirst, e){
+            let name = "";
+
+            if(isFirst){
+                name = e.target.value + " " + this.lName;
+            }
+            else{
+                name = this.fName + ' ' + e.target.value;
+            }
+
+            this.value = name;
+            this.$emit('input', this.value);
+          }
+        },
+        computed: {
+            fName(){
+                return this.value.split(" ")[0];
+            },
+            lName(){
+                return this.value.split(" ")[1];
+            }
+        }
+    }
+</script>
